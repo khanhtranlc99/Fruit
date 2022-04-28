c7912f5b277d50930719091e7689b21e0658a865
trankhanhlc <trankhanhlc99@gmail.com>
2022-04-28 19:29:22 +0700

    2020.3.15f2

---
+++ Assets/FruitSlicingGameTemplate/FSG_Scripts/MenuScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	//	GoolgeAdmob.RequestBanner();
+	}
+	public void removeAd(){
+	
+		Debug.Log("asdf");
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
